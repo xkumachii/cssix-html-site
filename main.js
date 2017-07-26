1f837afda1b4cfbe19f7c3b11456dda8357ad6d8
Marc Alejandro <malejandro@csumb.edu>
2017-07-25 17:30:54 -0700

    more changes and stuff like that

--- main.js
+++ main.js
@@ -5,7 +5,36 @@ function checkout (item1, item2, coupon) {
   var subtotal = item1 + item2;
   var couponValue = subtotal *coupon;
   var grandTotal = subtotal - couponValue;
-  console.log(grandTotal);
+  //console.log(grandTotal);
+  return grandTotal;
 }
 
-checkout(33, 19, .15);
+function helloFunction(name) {
+  if(name === 'sammy') {
+    console.log("screw you, man");
+  } else {
+    console.log("sup" + name + ".");
+  }
+};
+
+helloFunction('sammy');
+
+console.log(checkout(33, 19, .15));
+
+checkout(23, 0, 0);
+
+var oneItemResult = checkout(12, 0, 0);
+
+console.log(oneItemResult);
+
+checkout(500, 1000, .05);
+
+var bananaPrice = 1.5;
+var chocoPudding = 6;
+var superCoupon = .45;
+
+var bankAccount = 100; //fucking shit nigga god dam son
+
+var remainAmount = bankAccount - checkout(bananaPrice, chocoPudding, superCoupon);
+
+console.log('i have ' + remainAmount + " left in my bank account.");
